b6a7f2c93e73ec70a53ec3b2565cb00215076218
admin <tranthidathao_t62@hus.edu.vn>
2019-05-21 19:55:36 +0700

    234

--- index2.php
+++ index2.php
@@ -17,14 +17,21 @@ if (isset($_POST["height"]) && isset($_POST["weight"])) {
     $bmi = $weight/($height*$height);
     echo "Chỉ sô BMI là : " . $bmi;
 }
-if($bmi<18.5)
-    echo "Thiếu cân";
-elseif ($bmi>=18.50&&$bmi<=19.55)
-    echo "bình thường";
-elseif ($bmi>=23.00 && $bmi<=24.99)
-    echo "Thừa cân";
-elseif ($bmi>=25)
-    echo "béo";
+echo "<br>"
+ if($bmi < 18.5){
+            echo "Thiếu cân";
+        } elseif ($bmi >=18.5 && $bmi <= 22.99){
+            echo "Bình thường";
+        } elseif ($bmi >=23 && $bmi <= 24.99){
+            echo "Thừa cân";
+        } elseif ($bmi >= 25 && $bmi <= 29.99){
+            echo "Béo phì độ I";
+        } elseif ($bmi >= 30 && $bmi <= 39.99){
+            echo "Béo phì độ II";
+        } elseif ($bmi >= 40){
+            echo "Béo phì độ III";
+        }
+    }
 
 
 ?>
