03870bbd678d60f5b440a830891cf11715f8b757
admin <tranthidathao234@gmail.com>
2019-05-21 20:47:24 +0700

    234

---
+++ get2.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 5/21/2019
+ * Time: 08:24 PM
+ */
+//URL http://localhost:8080/thao2341998/index4.php?height=1.49&weight=42&calc=
+//nhưng tham số xuất hiện sau ? có thể láy đc bằng $_GET
+//Các tham số sau ? phân  tách bởi &
+echo"<pre>";
+print_r($_GET);
+echo "</pre>";
